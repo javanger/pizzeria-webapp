d15b83c1853f6892aaee3318ebf2621f79844beb
Guillaume <guillaume.gobert@gmail.com>
2018-03-20 17:33:22 +0100

    ajouter et lister les clients

--- src/main/java/dev/pizzeria/controller/ClientController.java
+++ src/main/java/dev/pizzeria/controller/ClientController.java
@@ -3,6 +3,10 @@ package dev.pizzeria.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.pizzeria.dao.ClientDao;
+import dev.pizzeria.dao.IClientDao;
+import dev.pizzeria.model.Client;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +16,8 @@ import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -19,41 +25,82 @@ import java.util.stream.Collectors;
  */
 public class ClientController extends HttpServlet {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
 
-    /**
-     * Page HTML de la réponse en cas d'insertion effectuée.
-     * Fichier présent dans le répertoire src/main/resources.
-     */
-    public static final String TEMPLATE_CLIENT_INSERE = "templates/client_insere.html";
+	/**
+	 * Page HTML de la réponse en cas d'insertion effectuée. Fichier présent
+	 * dans le répertoire src/main/resources.
+	 */
+	public static final String TEMPLATE_CLIENT_INSERE = "templates/client_insere.html";
+	public static final String TEMPLATE_LISTE_CLIENT = "templates/liste_client.html";
+	IClientDao dao = new ClientDao();
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		// récupération des paramètres
+		String nom = req.getParameter("nom");
+		String prenom = req.getParameter("prenom");
+		String ville = req.getParameter("ville");
+		String age = req.getParameter("age");
 
-        // récupération du paramètre nom
-        // <input name="nom">
-        String nom = req.getParameter("nom");
-        
-        LOGGER.info("Paramètre nom reçu " + nom);
+		LOGGER.info("Paramètre nom reçu " + nom);
+		LOGGER.info("Paramètre prenom reçu " + prenom);
+		LOGGER.info("Paramètre ville reçu " + ville);
+		LOGGER.info("Paramètre age reçu " + age);
 
+		// TODO insérer un nouveau client en base de données
+		Client nouveauClient = new Client(nom, prenom, ville, Integer.parseInt(age));
+		dao.saveNewClient(nouveauClient);
 
-        // TODO insérer un nouveau client en base de données
+		try {
+			// réponse au format UTF-8 pour le support des accents
+			resp.setCharacterEncoding("UTF-8");
 
+			// récupération du contenu du fichier template
+			String template = Files
+					.readAllLines(
+							Paths.get(this.getClass().getClassLoader().getResource(TEMPLATE_CLIENT_INSERE).toURI()))
+					.stream().collect(Collectors.joining());
 
-        try {
-            // réponse au format UTF-8 pour le support des accents
-            resp.setCharacterEncoding("UTF-8");
+			// écriture dans le corps de la réponse
+			PrintWriter writer = resp.getWriter();
+			writer.write(template);
 
-            // récupération du contenu du fichier template
-            String template = Files.readAllLines(Paths.get(this.getClass().getClassLoader().getResource(TEMPLATE_CLIENT_INSERE).toURI())).stream().collect(Collectors.joining());
+		} catch (URISyntaxException e) {
+			LOGGER.error("Fichier HTML non trouvé", e);
+		}
+	}
 
-            // écriture dans le corps de la réponse
-            PrintWriter writer = resp.getWriter();
-            writer.write(template);
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Client> lesClients = dao.findAllClients();
 
-        } catch (URISyntaxException e) {
-           LOGGER.error("Fichier HTML non trouvé", e);
-        }
-    }
+		String clientData = lesClients.stream()
+				.map(c -> String.format("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", c.getNom(),
+						c.getPrenom(), c.getVille(), c.getAge()))
+				.collect(Collectors.joining());
+
+		if (lesClients.isEmpty()) {
+			LOGGER.error("Liste vide");
+		}
+
+		try {
+			// réponse au format UTF-8 pour le support des accents
+			resp.setCharacterEncoding("UTF-8");
+
+			// récupération du contenu du fichier template
+			String template = Files
+					.readAllLines(
+							Paths.get(this.getClass().getClassLoader().getResource(TEMPLATE_LISTE_CLIENT).toURI()))
+					.stream().collect(Collectors.joining());
+			String superTemplate = String.format(template, clientData);
+			// écriture dans le corps de la réponse
+			PrintWriter writer = resp.getWriter();
+			writer.write(superTemplate);
+
+		} catch (URISyntaxException e) {
+			LOGGER.error("Fichier HTML non trouvé", e);
+		}
+	}
 }
---
+++ src/main/java/dev/pizzeria/model/Pizza.java
@@ -0,0 +1,187 @@
+package dev.pizzeria.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//import
+import dev.pizzeria.utils.*;
+
+/**
+ * @author GOBERT Guillaume Classe Pizza
+ */
+@Entity
+@Table(name = "pizza")
+public class Pizza {
+
+	// Attributes
+	/** id : int */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	/** code : String */
+	@ToString
+	@Column(name = "code", length = 4, nullable = false)
+	private String code;
+	/** libelle : String */
+	@ToString(separateur1 = " => ", UpperCase = true)
+	@Column(name = "libelle", length = 50, nullable = false)
+	private String libelle;
+	/** prix : double */
+	@ToString(separateur1 = " (", separateur2 = "�) ")
+	@Column(name = "prix", nullable = false)
+	private Double prix;
+	/** categorie : CategoriePizza */
+	@ToString(LowerCase = true, separateur1 = " => ")
+	@Column(name = "categorie", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private CategoriePizza categorie;
+
+	/**
+	 * Constructeur vide
+	 * 
+	 */
+	public Pizza() {
+
+	}
+
+	/**
+	 * Constructeur complet
+	 * 
+	 * @param id
+	 * @param code
+	 * @param libelle
+	 * @param prix
+	 * @param categorie
+	 */
+	public Pizza(int id, String code, String libelle, double prix, CategoriePizza categorie) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+		this.categorie = categorie;
+	}
+
+	/**
+	 * Constructeur
+	 * 
+	 * @param code
+	 * @param libelle
+	 * @param prix
+	 * @param categorie
+	 */
+	public Pizza(String code, String libelle, double prix, CategoriePizza categorie) {
+		super();
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+		this.categorie = categorie;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param code
+	 *            the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the libelle
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param libelle
+	 *            the libelle to set
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the prix
+	 */
+	public double getPrix() {
+		return prix;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param prix
+	 *            the prix to set
+	 */
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the categorie
+	 */
+	public CategoriePizza getCategorie() {
+		return categorie;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param categorie
+	 *            the categorie to set
+	 */
+	public void setCategorie(CategoriePizza categorie) {
+		this.categorie = categorie;
+	}
+
+	@Override
+	public String toString() {
+
+		return StringUtils.AffichagePizza(this);
+	}
+}
